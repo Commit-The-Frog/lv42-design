4414c43d8a134b84c6c399ede1d60fde6a078bbb
minjacho42 <minjacho@student.42seoul.kr>
2024-03-12 22:41:44 +0900

    feat: user info viewport demo complete

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
 	console.log(req);
-	// res.send('fuck')
 	res.sendFile('index.html');
 })
 
 // 3000 포트로 서버 오픈
-app.listen(3000, function() {
+app.listen(4219, function() {
     console.log("start! express server on port 3000")
 })
 
