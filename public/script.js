1767da6cf380be3bda96bf22708318525f2843bf
rillmo <maxman306@gmail.com>
2024-03-13 09:38:19 +0000

    almost finished

---
+++ public/script.js
@@ -0,0 +1,44 @@
+window.onload = function() {
+	document.getElementById("xbox-tab").className += " content-tab-active";
+	document.getElementById("xbox-content").className += " content-list-active";
+};
+
+function changeTab(tabId) {
+	const contentTabs = document.querySelectorAll(".content-tab");
+	for(var i=0; i<contentTabs.length; i++) {
+		contentTabs[i].className = "content-tab";
+	}
+	const contentLists = document.querySelectorAll(".content-lists");
+	for (var i=0; i<contentLists.length; i++) {
+		contentLists[i].className = "content-lists";
+	}
+	document.getElementById(tabId + "-tab").className += " content-tab-active";
+	document.getElementById(tabId + "-content").className += " content-list-active";
+}
+
+var cancelModal = document.getElementById("cancelModal");
+var reservationModal = document.getElementById("reservationModal");
+var missingModal = document.getElementById("missingModal");
+
+function showModal(modal) {
+	if (modal === "cancelModal")
+		cancelModal.style.display = "block";
+	else if (modal === "reservationModal")
+		reservationModal.style.display = "block";
+	else if (modal === "missingModal")
+		missingModal.style.display = "block";
+}
+
+function closeModal(modal) {
+	if (modal === "cancelModal")
+		cancelModal.style.display = "none";
+	else if (modal === "missingModal")
+		missingModal.style.display = "none";
+}
+
+window.onclick = function(event) {
+	if (event.target == cancelModal)
+		cancelModal.style.display = "none";
+	else if (event.target == reservationModal)
+		reservationModal.style.display = "none";
+}
