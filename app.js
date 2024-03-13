d4c6c6aef1888bbab197a339012336b890c45a32
minjacho <minjacho@student.42seoul.kr>
2024-03-13 08:14:20 +0000

    slide left right added

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
+app.get('/page1', function(req, res){
 	console.log(req);
-	res.sendFile('user_reservation_1.html');
+	res.sendFile(__dirname + '/public/user_reservation_1.html');
+})
+
+app.get('/page2', function(req, res){
+	console.log(req);
+	res.sendFile(__dirname + '/public/user_reservation_2.html');
+})
+
+app.get('/', function(req, res){
+	console.log()
+	res.sendFile('index.html');
 })
 
 // 3000 포트로 서버 오픈
