8570149f8ebfeca9b5302a98bdaf16f303d8955d
김준호 <junkim2@student.42seoul.kr>
2024-04-18 21:51:25 +0900

    added repair-page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    fontFamily: {
+      "noto" : ['Noto Sans KR', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
