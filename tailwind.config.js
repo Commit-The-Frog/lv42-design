1b78a21408c6a5570e30d0058b2abc1961d86fc1
rillmo <maxman306@gmail.com>
2024-04-19 18:46:46 +0000

    fixed bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,12 @@ module.exports = {
         'logo': "url('../img/logo/logo.svg')",
         'logo-hazy': "url('../img/logo/logo-darkened.svg')",
         'default': "url('../img/background.svg')",
+        'default-desktop': "url('../img/background-desktop.svg')",
         'xbox': "url('../img/xbox.png')",
         'switch': "url('../img/switch.png')",
         'ps5': "url('../img/ps5.png')",
         'controller-icon': "url('../img/controller-icon.svg')",
-        'ps5-btn-info': "url('../img/ps5-button-info.png')"
+        'ps5-btn-info': "url('../img/ps5-button-info.png')",
       }
     },
   },
